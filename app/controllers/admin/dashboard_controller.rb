36f9e6dfb94ab5dc158222ccf5c35dbadc8ebd2c
Erica <ericasu33@gmail.com>
2021-01-05 21:58:43 -0800

    rename instance variable and add alt text to image

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,6 +2,6 @@ class Admin::DashboardController < ApplicationController
    http_basic_authenticate_with name: "Jungle", password: "book"
   def show
     @products = Product.count
-    @category = Category.count
+    @categories = Category.count
   end
 end
