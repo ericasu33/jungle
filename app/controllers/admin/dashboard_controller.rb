a196d3dcfabc86a22b5fcf7a586fed5b2d268280
Erica <ericasu33@gmail.com>
2021-01-06 16:42:31 -0800

    modify http_basic_authenticate to use env vars

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,6 @@
 class Admin::DashboardController < ApplicationController
-   http_basic_authenticate_with name: "Jungle", password: "book"
+   http_basic_authenticate_with name: ENV['HTTP_BASIC_USER'],
+                                password: ENV['HTTP_BASIC_PASSWORD']
   def show
     @products = Product.count
     @categories = Category.count
